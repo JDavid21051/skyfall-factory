bbd6d006c71d84402740034d61dec8f6f4482032
JDavid21051 <david.pelaezcumbe@gmail.com>
2024-02-01 23:57:03 -0500

    * test for table-nested.component

--- projects/ngx-table-nested/src/lib/components/table-nested/table-nested.component.spec.ts
+++ projects/ngx-table-nested/src/lib/components/table-nested/table-nested.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableNestedComponent } from './table-nested.component';
+import {Component} from '@angular/core';
 
-describe('TableNestedComponent', () => {
-  let component = new TableNestedComponent<any>();
-  let fixture: ComponentFixture<TableNestedComponent<any>>;
+describe('Unit test for TableNestedComponent', () => {
+  let component = new TableNestedComponent<{}>();
+  let fixture: ComponentFixture<TableNestedComponent<{}>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,7 +13,7 @@ describe('TableNestedComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TableNestedComponent);
+    fixture = TestBed.createComponent<TableNestedComponent<{}>>(TableNestedComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,4 +21,31 @@ describe('TableNestedComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // toggles row expansion when row is not expanded
+  it('should toggle row expansion when row is not expanded', () => {
+    const component = new TableNestedComponent<{}>();
+    const row = { childrenKey: [], expanded: false };
+
+    component.toggleExpandRow(row);
+
+    expect(component.table.rowDetail.toggleExpandRow).toHaveBeenCalledWith(row);
+    expect(component.tableRowExpanded).toEqual(row);
+    expect(component.tableRowsCount).toEqual(row.childrenKey.length);
+  });
+
+  // Returns the correct height when tableRowsCount is 0.
+  it('should return 0 when tableRowsCount is 0', () => {
+    const component = new TableNestedComponent();
+    component.childrenRowOff = 0;
+    component.tableRowsCount = 0;
+    expect(component.getDetailHeight()).toBe(0);
+  });
+
+  it('should return 0 when tableRowsCount is 0', () => {
+    const component = new TableNestedComponent();
+    component.tableRowsCount = 0;
+    expect(component.calcRowCount()).toBe(0);
+  });
+
 });
