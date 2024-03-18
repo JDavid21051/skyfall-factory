79a025e79979376c799e62ed2ebdb0d16d358bc7
JDavid21051 <david.pelaezcumbe@gmail.com>
2024-03-17 22:05:42 -0500

    * pruebas funcionales * karma + jasmine * test script

--- projects/ngx-table-nested/src/lib/components/atoms/table-action-button/table-action-button.component.spec.ts
+++ projects/ngx-table-nested/src/lib/components/atoms/table-action-button/table-action-button.component.spec.ts
@@ -1,17 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TableActionButtonComponent } from './table-action-button.component';
+import { ActionConfigInterface } from '../../../interfaces/tree-nested.models';
 
-describe('TableActionButtonComponent', () => {
+fdescribe('TableActionButtonComponent', () => {
   let component: TableActionButtonComponent;
   let fixture: ComponentFixture<TableActionButtonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ TableActionButtonComponent ]
-    })
-    .compileComponents();
-
+  beforeEach(() => {
     fixture = TestBed.createComponent(TableActionButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +14,37 @@ describe('TableActionButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render button with provided data', () => {
+    const actionConfig: ActionConfigInterface = {
+      label: 'Test Label',
+      class: 'a',
+      tooltip: 'Test Tooltip',
+      icon: { icon: 'test_icon', pack: 'eva' },
+      click: () => { console.log('Test click');
+      }
+    };
+
+    component.data = actionConfig;
+    fixture.detectChanges();
+    const buttonElement: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+
+    expect(buttonElement).toBeTruthy();
+    expect(buttonElement.textContent).toContain(actionConfig.label);
+    expect(buttonElement.title).toBe(actionConfig.tooltip);
+    const selectorRes = buttonElement.querySelector('.material-symbols-outlined');
+    if (selectorRes) {
+      expect(selectorRes.textContent).toContain(actionConfig.icon.icon);
+    } else {
+      expect(selectorRes).toBeNull();
+    }
+  });
+
+  it('should not render button when data is undefined', () => {
+    component.data = undefined;
+    fixture.detectChanges();
+    const buttonElement: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    expect(buttonElement).toBeNull();
   });
 });
--- projects/ngx-table-nested/src/lib/components/table-basic/table-basic.component.spec.ts
+++ projects/ngx-table-nested/src/lib/components/table-basic/table-basic.component.spec.ts
@@ -3,18 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TableBasicComponent } from './table-basic.component';
 
 describe('TableBasicComponent', () => {
-  let component: TableBasicComponent;
-  let fixture: ComponentFixture<TableBasicComponent>;
+  let component: TableBasicComponent<any>;
+  let fixture: ComponentFixture<TableBasicComponent<any>>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ TableBasicComponent ]
-    })
-    .compileComponents();
 
     fixture = TestBed.createComponent(TableBasicComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
+    component = fixture.componentInstance;
+    TestBed.createComponent(TableBasicComponent<any>)
   });
 
   it('should create', () => {
